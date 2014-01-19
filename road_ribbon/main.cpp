a65c688f558ac415a0e8086e58c4f09ff44184ab
Artem Shoobovych <shybovycha@gmail.com>
2014-01-19 21:27:07 +0200

    Started documenting

--- road_ribbon/main.cpp
+++ road_ribbon/main.cpp
@@ -24,8 +24,11 @@ int main(int argc, char *argv[])
 
     PlainRoadBuilder roadBuilder(parser.centers, 15.0);
 
-    Camera camera; //(Vector3d(0, 30, 10), Vector3d(0, 0, 0.0));
+    Camera camera;
+
     roadBuilder.setUpCamera(camera);
+
+    glShadeModel(GL_SMOOTH);
 
     while (window.isOpen())
     {
